24a89920d5f31e9d1aaacd584d83835428507e3d
Malcolm Mohr <mohr6@wisc.edu>
2026-07-05 22:19:30 -0500

    diff clock routing

--- QubitSim/blochSphereGUI.py
+++ QubitSim/blochSphereGUI.py
@@ -13,17 +13,19 @@ class BlochSphereGUI:
     def __init__(self):
         self.qubit = QubitSystem(2 * math.pi)
 
-        self.fig = plt.figure(figsize=(8, 9))
+        self.fig = plt.figure(figsize=(8, 9.5))
         self.ax = self.fig.add_subplot(111, projection="3d")
-        self.fig.subplots_adjust(bottom=0.32)
+        self.fig.subplots_adjust(bottom=0.37)
 
         self._draw_static_sphere()
         self.trail_line, = self.ax.plot([], [], [], color="tab:red", linewidth=1)
+        self.axis_line, = self.ax.plot([], [], [], color="tab:blue", linewidth=1.5, linestyle="--")
         self.arrow = None
         self.trail = []
         self._draw_state()
 
         self._build_controls()
+        self._update_axis_line()
 
     def _draw_static_sphere(self):
         ax = self.ax
@@ -67,15 +69,27 @@ class BlochSphereGUI:
         self.trail_line.set_data(trail_arr[:, 0], trail_arr[:, 1])
         self.trail_line.set_3d_properties(trail_arr[:, 2])
 
+    def _update_axis_line(self, event=None):
+        I = self.slider_i.val
+        Q = self.slider_q.val
+        phase = math.atan2(Q, I)
+        axis_len = 1.3
+        dx, dy = axis_len * math.cos(phase), axis_len * math.sin(phase)
+        self.axis_line.set_data([-dx, dx], [-dy, dy])
+        self.axis_line.set_3d_properties([0, 0])
+        self.fig.canvas.draw_idle()
+
     def _build_controls(self):
         axcolor = "whitesmoke"
-        ax_i = self.fig.add_axes([0.15, 0.20, 0.7, 0.03], facecolor=axcolor)
-        ax_q = self.fig.add_axes([0.15, 0.15, 0.7, 0.03], facecolor=axcolor)
-        ax_dur = self.fig.add_axes([0.15, 0.10, 0.7, 0.03], facecolor=axcolor)
+        ax_i = self.fig.add_axes([0.15, 0.27, 0.7, 0.03], facecolor=axcolor)
+        ax_q = self.fig.add_axes([0.15, 0.22, 0.7, 0.03], facecolor=axcolor)
+        ax_dur = self.fig.add_axes([0.15, 0.17, 0.7, 0.03], facecolor=axcolor)
+        ax_k = self.fig.add_axes([0.15, 0.12, 0.7, 0.03], facecolor=axcolor)
 
         self.slider_i = Slider(ax_i, "I", -1.0, 1.0, valinit=1.0)
         self.slider_q = Slider(ax_q, "Q", -1.0, 1.0, valinit=0.0)
         self.slider_dur = Slider(ax_dur, "Duration", 0.0, 1.0, valinit=0.25)
+        self.slider_k = Slider(ax_k, "Coupling k", 0.0, 4 * math.pi, valinit=2 * math.pi)
 
         ax_apply = self.fig.add_axes([0.15, 0.03, 0.3, 0.05])
         ax_reset = self.fig.add_axes([0.55, 0.03, 0.3, 0.05])
@@ -84,6 +98,9 @@ class BlochSphereGUI:
 
         self.btn_apply.on_clicked(self._on_apply)
         self.btn_reset.on_clicked(self._on_reset)
+        self.slider_i.on_changed(self._update_axis_line)
+        self.slider_q.on_changed(self._update_axis_line)
+        self.slider_k.on_changed(self._on_k_changed)
 
     def _on_apply(self, event):
         I = self.slider_i.val
@@ -98,8 +115,11 @@ class BlochSphereGUI:
             self._draw_state()
             plt.pause(FRAME_PAUSE)
 
+    def _on_k_changed(self, value):
+        self.qubit.k = value
+
     def _on_reset(self, event):
-        self.qubit = QubitSystem(2 * math.pi)
+        self.qubit = QubitSystem(self.slider_k.val)
         self.trail = []
         self._draw_state()
         plt.draw()
